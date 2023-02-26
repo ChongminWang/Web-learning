5fa11aa2f29cf6eb2719b5347fd996dfc9553079
2577624123 <2577624123@qq.com>
2023-02-26 17:21:20 +0800

    update

--- React/code/03_react脚手架/react_staging/src/App.jsx
+++ React/code/03_react脚手架/react_staging/src/App.jsx
@@ -2,21 +2,27 @@
  * @Author: 2577624123 2577624123@qq.com
  * @Date: 2023-02-16 11:24:56
  * @LastEditors: 2577624123 2577624123@qq.com
- * @LastEditTime: 2023-02-24 10:00:25
+ * @LastEditTime: 2023-02-24 19:16:55
  * @FilePath: \Web-learning\React\code\03_react脚手架\react_staging\src\App.jsx
  * @Description: 
  * 
  */
 import React, { Component } from 'react'
-
+import {Button} from 'antd'
+import 'antd/dist/reset.css'
 
 export default class App extends Component {
 
 	
 	render() {
-		// const {users} = this.state
 		return (
-			<div>00</div>
+			<div>
+				00
+				<button>点我</button>
+				<Button type='primary'>按钮1</Button>
+				<Button type='link'>按钮2</Button>
+				<Button >按钮3</Button>
+			</div>
 		)
 	}
 }
